c36a1a9d62372b356c89f77d96010841e6b2a9bb
Abhishek Mathur <132328327+AbhishekMathur25@users.noreply.github.com>
2025-04-29 14:54:05 +0530

    Update writer.py

--- writer.py
+++ writer.py
@@ -6,10 +6,10 @@ writer_agent = Agent(
     instructions="""
     You are a professional newsletter writer. 
     Based on the research content, write clear, engaging, and accurate summaries in a consistent tone. 
-    Use 5 lines paragraphs and make the content readable for a general audience. 
+    Use 5 lines paragraphs and make the content readable for a general audience, but it should be in detail. 
     should contains political events, economic developments, social issues, crime, and entertainment.
     Avoid repetition, and highlight key facts in bullet points where appropriate.
     """,
-    model=AzureOpenAI(id="gpt-4o-mini", api_version="2025-01-01"),
+    model=AzureOpenAI(id="gpt-4o-mini-techops", api_version="2025-01-01-preview"),
     search_knowledge=True,
 )
