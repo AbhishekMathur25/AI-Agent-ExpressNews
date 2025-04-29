6ca1a0e22076f90eb457111cd16529d5d8840eb1
Abhishek Mathur <132328327+AbhishekMathur25@users.noreply.github.com>
2025-04-29 14:54:27 +0530

    Update research.py

--- research.py
+++ research.py
@@ -8,6 +8,6 @@ from agno.tools.hackernews import HackerNewsTools
 research_agent = Agent(
     name="Research Agent",
     tools=[GoogleSearchTools(),HackerNewsTools()],
-    instructions="You are a research agent. Your task is to scrape top 3 news for today date in detail on political events, economic developments, social issues, crime, and entertainment.",
-    model=AzureOpenAI(id="gpt-4o-mini",api_version="2025-01-01"),
+    instructions="You are a research agent. Your task is to scrape top 3 news for today date in very detail on political events, economic developments, social issues, crime, and entertainment.",
+    model=AzureOpenAI(id="gpt-4o-mini-techops",api_version="2025-01-01-preview"),
 )
